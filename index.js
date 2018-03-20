17de405f1342159dc9c70ff4722fe0208c4abf32
Karl Amoguis <ktamoguis@gmail.com>
2018-03-20 08:22:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string){
     console.log("I can't hear you!")
   } else if (string === string.toUpperCase()){
     console.log("YES INDEED!")
-  } else if (string === "I love you, Grandma"){
+  } else if (string === "I love you, Grandma."){
     console.log("I love you, too.")
   }
 }
